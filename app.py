7d4f6681a97ea30337fbbfd929b09894faaa3f32
Juliya91 <juliyarezanovich@gmail.com>
2021-08-22 18:30:07 +0000

    chore: Add index.html and base.html

--- app.py
+++ app.py
@@ -8,12 +8,21 @@ if os.path.exists("env.py"):
     import env
 
 
-app = Flask(__name__)  
+app = Flask(__name__)
+
+# mondodb
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 
 @app.route("/")
 def hello():
-    return "Hello World ... again!"
+    habits = list(mongo.db.habits.find())
+    print('habits ', habits)
+    return render_template("index.html", habits=habits)
 
 
 if __name__ == "__main__":
