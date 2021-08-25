64dc06f1163bbbe8a919a5d3447b5577498993ac
Juliya91 <juliyarezanovich@gmail.com>
2021-08-25 17:29:04 +0000

    feat: Add sign up template and jquery for confirming the password

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -24,6 +25,11 @@ def get_habits():
     habits = list(mongo.db.habits.find())
     print('habits ', habits)
     return render_template("habits.html", habits=habits)
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    return render_template("signup.html")
 
 
 if __name__ == "__main__":
--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,15 @@
+// Code for sidenav taken from Materialize (https://materializecss.com/navbar.html)
+
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
+    // Code for confirm password taken from Online Web Tutor 
+    // (https://onlinewebtutorblog.com/how-to-validate-password-and-confirm-password-using-jquery/) and edited slightly
+    $("#confirm_password").on('keyup', function() {
+      let password = $("#password").val();
+      let confirmPassword = $("#confirm_password").val();
+      if (password != confirmPassword)
+      $("#CheckPassMatch").html("Password does not match !").css("color", "red");
+      else
+        $("#CheckPassMatch").html("Password match !").css("color", "green");
+    });
   });
